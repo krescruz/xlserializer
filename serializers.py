10e29da3c3a6b7d897b8934bf95aabd953c1084e
kresCruz <krescruz@gmail.com>
2015-03-30 18:10:25 -0600

    Get data by name columns defined

--- serializers.py
+++ serializers.py
@@ -1,13 +1,17 @@
 import xlrd
 from xlrd.biffh import XLRDError
+import copy
 
 
 class XlSerializerBase(object):	
+	
 	workbook = None
-	def __init__(self, path=None, init_row=0):
+
+	def __init__(self, path=None, init_row=0, sheet_name=(), declared_columns=()):
 		self.path = path
 		self.init_row = init_row
-		self.sheet_name = None
+		self.sheet_name = sheet_name
+		self.declared_columns = declared_columns
 		self._open()
 
 	def _open(self):
@@ -30,11 +34,21 @@ class XlSerializerBase(object):
 		row = self.worksheet.row(self.init_row)
 		return list(column.value for column in row)
 
+	def _get_index_col(self, name):
+		return self.column_names.index(name)
+
+	def _col_slice(self, col_index, start_rowx=0, end_rowx=None):
+		cols= self.worksheet.col_slice(col_index, start_rowx, end_rowx)
+		return cols
+
+	def get_data_rows(self):
+		data_rows = {}
+		column_names = self.declared_columns if self.declared_columns else self.column_names
+		for name in column_names:
+			col_index = self._get_index_col(name)
+			data_rows[name] = self._col_slice(col_index)
+		return data_rows
+
 	@property
 	def data(self):
-		num_rows = self.worksheet.nrows - 1
-		init_data = self.init_row + 1
-		return list(
-			self.worksheet.row(row)
-			for row in range(init_data, num_rows)
-		)
+		return self.get_data_rows()
